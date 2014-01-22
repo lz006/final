4d63f5189256953fbce8b47cf98c644a19f61e1b
Lucas <tuonoditech@web.de>
2014-01-22 16:09:41 +0100

    Kommentare Cell-Klassen

--- stundenplantool2/src/com/hdm/stundenplantool2/client/LehrveranstaltungCell.java
+++ stundenplantool2/src/com/hdm/stundenplantool2/client/LehrveranstaltungCell.java
@@ -2,12 +2,38 @@ package com.hdm.stundenplantool2.client;
 
 import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.hdm.stundenplantool2.shared.bo.Dozent;
 import com.hdm.stundenplantool2.shared.bo.Lehrveranstaltung;
 
+/**
+ * Diese Klasse ist eine konkrete Ausprägung der abstrakten Klasse
+ * AbstractCell<T>. Sie definiert den sichtbaren Inhalt einer Zelle
+ * des CellTrees für einen spezifischen Kind-Element-Typ, in diesem 
+ * Fall einem Kind-Element vom Typ Lehrveranstaltung {@link Lehrveranstaltung}. 
+ * Eine Instanz einer solchen Klasse wird vom Konstruktor der 
+ * DefaultNodeInfo<T>-Klasse verlangt und wird diesem konkret in
+ * der Methode "getNodeInfo(T value)" in der Klasse "CustomTreeViewModel"
+ * {@link CustomTreeViewModel} als Argument übergeben.
+ * 
+ * @author Roth, Klatt, Zimmermann, Moser, Sonntag, Zanella
+ * @version 1.0
+ * 
+ */
 public class LehrveranstaltungCell extends AbstractCell<Lehrveranstaltung> {
 
-	public void render(Context context, Lehrveranstaltung value,
-			SafeHtmlBuilder sb) {
+	/**
+	 * Abstrakte Methode der Klasse AbstractCell<T> welche nicht-abstrakt überschrieben
+	 * werden muss. Ihr wird u.a. ein Objekt eines definierten Kind-Element-Typs als
+	 * Argument übergeben und gewünschte Attributwerte dessen, werden innerhalb eines
+	 * <div>-Tags gesetzt. Diese Attributwerte ergeben jenen Text, welcher in den Zellen
+	 * des CellTrees schlißlich sichtbar ist.
+	 * 
+	 *  @param	Context-Objekt, welches Kontextinformationen enthält
+	 *  		Lehrveranstaltung-Objekt, welches den definierten Kind-Element-Typ enthält
+	 *  		SafeHtmlBuilder-Objekt, welches zuständig für die Erzeugung von
+	 *  		anzeigbaren HTML-Tags benötigt wird
+	 */
+	public void render(Context context, Lehrveranstaltung value, SafeHtmlBuilder sb) {
 
 		if (value == null) {
 			return;
